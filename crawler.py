e09d8080c49d992736322ceb0c7720760fbdb034
joey8angelo <joey8angelo@gmail.com>
2025-05-04 01:06:12 -0700

    cleanup, fix 429 http error, comments

--- crawler.py
+++ crawler.py
@@ -15,10 +15,6 @@ import re #package for reg ex expressions
 # CLIENT_SECRET="some_secret_key"...
 load_dotenv()
 
-# Exception raised for errors in the parsing process.
-class ParseError(Exception): 
-    pass
-
 # Crawler class handles each Spider thread
 # Each Spider has its own memory space, with some shared data
 # Spiders can send data to each other through a shared list of queues
@@ -27,16 +23,22 @@ class ParseError(Exception):
 # hash the subreddit name and send the **url** to the corresponding queue
 #                                      ^^^^^^^
 class Crawler:
-    def __init__(self, seed_file, num_pages, hops_away, output_dir, 
+    def __init__(self, seed_file, num_pages, size_limit, output_dir, 
                  num_procs, timeout, debug=False):
         # Each process has its own memory space
         # all self.* variables are NOT shared data
         # shared data is explicitly created
         
+        start_time = time.time()
+        start_size = size_limit
+
+        if num_pages is None:
+            num_pages = 9_223_372_036_854_775_807
+
         self.seed_file  = seed_file # file with reddit urls to start from
         # num_pages is shared data. The total number of pages to be processed
-        self.num_pages  = Value('i', num_pages)
-        self.hops_away  = hops_away # max hops from the seed file
+        self.num_pages  = Value('q', num_pages)
+        self.size_limit = Value('q', size_limit) # total amount of data to store
         self.output_dir = output_dir # directory to save files
         self.num_procs  = num_procs # number of processes to spawn
         self.timeout    = timeout # timeout in seconds for each proc
@@ -48,8 +50,7 @@ class Crawler:
         manager         = Manager()
         self.queues     = [manager.Queue() for _ in range(num_procs)]
         self.reddit     = self.get_reddit() # PRAW API
-        self.sleep_time = 0.5 # time to sleep if the queue is empty
-        self.sub_map = {} #needed for hashmap
+        self.sleep_time = 5 # time to sleep if the queue is empty
         self.load_seeds()
 
         # Each proc runs the spider method with its thread_id(i)
@@ -66,12 +67,16 @@ class Crawler:
 
         if self.num_pages.value <= 0:
             print("All pages processed.")
+        elif self.size_limit.value <= 0:
+            print("Size limit reached.")
         else:
             print("All threads done")
 
+        sz = start_size - self.size_limit.value
+        print(f"Processed {sz} bytes in {time.time() - start_time:.2f} seconds")
+
     # Create a PRAW Reddit instance using credentials from .env file
-    def get_reddit(self):
-        
+    def get_reddit(self): 
         reddit = praw.Reddit(
             client_id=os.getenv("CLIENT_ID"),
             client_secret=os.getenv("CLIENT_SECRET"),
@@ -89,9 +94,6 @@ class Crawler:
         return reddit
 
     # Load the seed urls from the seed file and assign them to Spiders
-    # right now simply puts them in each queue in order
-    # TODO: each thread should be in charge of a specific subreddit,
-    # hash the url and send it to the correct queue
     def load_seeds(self):
         if not os.path.exists(self.seed_file):
             print(f"Seed file {self.seed_file} does not exist.")
@@ -99,8 +101,8 @@ class Crawler:
         with open(self.seed_file, "r") as f:
             seeds = f.readlines()
         seeds = [seed.strip() for seed in seeds]
-        for i, seed in enumerate(seeds):
-            self.queues[i%self.num_procs].put(seed)
+        for seed in seeds:
+            self.queues[self.hash(seed)].put(seed)
         print(f"Loaded {len(seeds)} seeds from {self.seed_file}.")
 
     # Each Spider runs this method
@@ -112,7 +114,7 @@ class Crawler:
         print(f"Thread {thread_id} started.")
         # While there are still pages to process
         # no lock is aquired to just read the value
-        while self.num_pages.value > 0:
+        while self.num_pages.value > 0 and self.size_limit.value > 0:
             try:
                 # Queue.get() will raise an exception if the queue is empty
                 # block=False means that the thread will not stall waiting
@@ -135,99 +137,94 @@ class Crawler:
 
     # Parse a given url, make a submission object to start parsing data
     def parse_url(self, url):
-        # parse all the links in the post and comments, add them
-        # to the queues and reduce hops_away, do not add any
-        # links to the queue if hops_away == 0
-        try:
-            if self.debug:
-                print(f"Thread {self.thread_id} processesing URL: {url}")
-            
-            self.parse_submission(self.reddit.submission(url=url))
-
-            # if we successfully got the submission and data,
-            # count this page as processed
-            with self.num_pages.get_lock():
-                self.num_pages.value -= 1
+        # only try again if we get a 429 error for too many requests
+        while True:
+            try:
+                if self.debug:
+                    print(f"Thread {self.thread_id} processesing URL: {url}")
            
-            # how to send data to other threads
-            # if self.thread_id == 1:
-            #     self.queues[0].put(url)
+                self.parse_submission(self.reddit.submission(url=url))
+
+                # if we successfully got the submission and data,
+                # count this page as processed
+                with self.num_pages.get_lock():
+                    self.num_pages.value -= 1
+            
+                break
         
-        except ParseError as e:
-            print(f"Thread {self.thread_id} ParseError: {e}")
-        except Exception as e:
-            print(f"Thread {self.thread_id} error processing {url}: {e}")
+            except Exception as e:
+                if str(e) == "received 429 HTTP response":
+                    print(f"Thread {self.thread_id} rate limited. Sleeping...")
+                    time.sleep(2)
+                    continue
+                print(f"Thread {self.thread_id} error processing {url}: {e}")
+                break
 
-    # TODO: Parse a reddit post:
-    # gets all the data and saves it to a file
-    # extracts all the links from the post and comments and gives them
-    # to their corresponding queues
-    # Raises ParseErrors if something goes wrong
-    # If a comment or post has a link to a subreddit instead of a post,
-    # do something like get the 100 newest posts, or 100 hottes posts, etc
-    #extracting all the urls from a post/comment
-
+    # Given some text extract all the subreddits in the form r/subreddit_name
     def extract_subreddits(self, text: str) -> list[str]:
-        pattern = re.compile(r'/r/([A-Za-z0-9_]+)')
+        pattern = re.compile(r'r/([A-Za-z0-9_]+)')
         # findall returns a list of capture‐group strings
         return pattern.findall(text)
     
+    # Given some text extract all the urls in the form (http://*) or (https://*)
     def extract_urls(self, text: str) -> list[str]:
         URL_REGEX = re.compile(
             r'(https?://[^\s]+)|(www\.[^\s]+)',
             flags=re.IGNORECASE
         )
-        matches = URL_REGEX.findall(text)
-        return [m[0] or m[1] for m in matches]
+        return URL_REGEX.findall(text)
     
+    # Hash a value to determine which queue to send it to
+    def hash(self, value):
+        return abs(hash(value)) % self.num_procs
+
+    # Parse reddit comment, extract subreddits and links
+    def parse_comment(self, comment, comments_out, links_out):
+        subreddits = self.extract_subreddits(comment.body)
+        links = self.extract_urls(comment.body)
+        for subreddit in subreddits:
+            subreddit = self.reddit.subreddit(subreddit)
+            posts = subreddit.new(limit=100)
+            for post in posts:
+                self.queues[self.hash(post.url)].put(post.url)
+        for link in links:
+            try:
+                # if the link is not a reddit post
+                # this will raise an error
+                submission = self.reddit.submission(url=link)
+                subreddit = submission.subreddit.display_name
+                self.queues[self.hash(subreddit)].put(submission.url)
+            except Exception as e:
+                links_out.append(link)
+
+        comments_out.append(comment.body)
+
+    # parse a reddit post and save it to a file, saves:
+    # ID, Subreddit, Author, Timestamp, Title, Post text, URL,
+    # Comments, and links to non reddit.com websites
     def parse_submission(self, submission):
-        postData= { #download data and info
-        "id":          submission.id,
-        "subreddit":   submission.subreddit.display_name,
-        "author":      str(submission.author),
-        "created_utc": submission.created_utc,
-        "title":       submission.title,
-        "selftext":    submission.selftext,
-        "url":         submission.url,
+        comments = []
+        external_links = []
+        if self.debug:
+            print(f"Thread {self.thread_id} parsing comments")
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            self.parse_comment(comment, comments, external_links)
+        if self.debug:
+            print(f"Thread {self.thread_id} finished parsing comments")
+
+        postData = { #download data and info
+            "id":             submission.id,
+            "subreddit":      submission.subreddit.display_name,
+            "author":         str(submission.author),
+            "created_utc":    submission.created_utc,
+            "title":          submission.title,
+            "selftext":       submission.selftext,
+            "url":            submission.url,
+            "comments":       comments,
+            "external_links": external_links 
         }
         self.save_to_json(postData) #call saving function
-
-        quene = []
-        try:
-            submission.comments.replace_more(limit=0)
-            comments = [comment.body for comment in 
-                        submission.comments.list()]
-            #extract all the links from posts and comments
-            for comment in comments:
-                subreddits = self.extract_subreddits(comment)
-                external_links = self.extract_urls(comment)
-                print("REDDIT",external_links)
-                quene.append(external_links)
-
-                for subreddit in subreddits:
-                    #print("SUBREDDIT", subreddit) #for testing what subreddits are being grabbed
-                    key = subreddit.lower() #map name to key(.lower to avoid same reddits)
-                    if key not in self.sub_map: #assign hash map iif sub has not been seen
-                        self.sub_map[key] = hash(key) % len(self.queues)
-                    qIndex = self.sub_map[key] #assign queue Index to the key
-
-                    for link in external_links: 
-                        self.queues[qIndex].put(link)  #added queue index of link to enqueue
-
-                    subreddit = self.reddit.subreddit(subreddit)
-                    posts = subreddit.new(limit=100)  # Fetch newest 100 posts
-                    for post in posts: #now queues the post onto the same spyder
-                        self.queues[qIndex].put(post.url)
-
-                    # if self.debug:
-                    #     print(f"[T{self.thread_id}] queued {post.url} → queue {qIndex}")
-                    print("POSTS", posts)
-        #raising error if something goes wrong
-        except Exception as e:
-            raise  ParseError(str(e))
-
-        if self.debug:
-            print(f"Thread {self.thread_id} saved post: {submission.id}")
 
     def get_html_title(self, url):
         try:
@@ -242,13 +239,32 @@ class Crawler:
         return None
 
     def save_to_json(self, post_dict):
+        if self.debug:
+            print(f"Thread {self.thread_id} saving data to JSON")
         os.makedirs(self.output_dir, exist_ok=True)
         file_prefix = os.path.join(self.output_dir, "reddit_data")
         file_index = 0
-        while os.path.exists(f"{file_prefix}_{file_index}.json") and os.path.getsize(f"{file_prefix}_{file_index}.json") >= 10 * 1024 * 1024:
+        while os.path.exists(f"{file_prefix}_{file_index}.json") and os.path.getsize(f"{file_prefix}_{file_index}.json") >= 10*1024*1024:
             file_index += 1
         with open(f"{file_prefix}_{file_index}.json", 'a', encoding='utf-8') as f:
             json.dump(post_dict, f)
             f.write("\n")
+
+        # size of this json object in bytes
+        # removes the size from the size limit
+        jsz = len(json.dumps(post_dict).encode('utf-8'))
+        with self.size_limit.get_lock():
+            self.size_limit.value -= jsz
+
+        print(f"Data remaining: {self.size_limit.value} bytes")
+
 if __name__ == "__main__":
-    crawler = Crawler("seeds.txt", 100, 2, "output", 2, 2, True)
+    crawler = Crawler(
+                seed_file="seeds.txt",
+                num_pages=None,
+                size_limit=10*1024*1024*1024, # 10 GB
+                output_dir="output",
+                num_procs=16,
+                timeout=5*60, # 5 minutes
+                debug=False
+            )
