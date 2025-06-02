1670f5178f04ad7b9d55c52bf3fb1a79e573ed38
Sreeja Pagireddy <sreeja@karibu.ai>
2025-06-01 20:32:32 -0700

    finished the reddit_indexer

--- reddit_indexer.py
+++ reddit_indexer.py
@@ -28,9 +28,10 @@ class redditIndexer:
         #create the Lucene index, got from online
         conf = IndexWriterConfig(StandardAnalyzer())
         conf.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+        #creating a new index
         self.writer = IndexWriter(FSDirectory.open(Paths.get(index_dir)), conf)
-    def json_indexes():
-         for json_file in os.listdir(self.json):      
+    def json_indexes(self):
+        for json_file in os.listdir(self.json):      
             #get the path of the json
             json_path = os.path.join(self.json, json_file)
             if not os.path.exists(json_path):
@@ -38,6 +39,13 @@ class redditIndexer:
             with open(json_path, "r", encoding="utf-8") as f:
                  for line in f:
                       data = json.loads(line)
-                      #creating document
+                      #adding documents to the index
                       document = Document()
-                      
+                      document.add(StringField("author", data.get("author", ""), StringField.Store.YES))
+                      document.add(StringField("timestamp", data.get("created_utc", ""), StringField.Store.YES))
+                      document.add(StringField("selftext", data.get("selftext", ""), StringField.Store.YES))
+                      document.add(TextField("comments", "\n".join(data.get("comments", []), TextField.Store.NO)))
+                      self.writer.addDocument(document)
+        self.writer.close()
+
+
